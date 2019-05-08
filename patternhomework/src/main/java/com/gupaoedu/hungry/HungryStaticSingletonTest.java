20995047d119e29f96268e85da4db8083f2e40ee
shiboyue <shiboyue@126.com>
2019-05-08 17:59:30 +0800

    001

--- patternhomework/src/main/java/com/gupaoedu/hungry/HungryStaticSingletonTest.java
+++ patternhomework/src/main/java/com/gupaoedu/hungry/HungryStaticSingletonTest.java
@@ -4,7 +4,9 @@ public class HungryStaticSingletonTest {
 
     public static void main(String[] args) {
 
+		System.out.println("Hello World 001");
         HungrySingleton.getInstance()
+		
         System.out.println("Hello World");
     }
 }
